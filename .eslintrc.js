2f8cdbde633e45b640f4c712018410e2f561cd54
Carlos Guimaraes <sekmet@open4g.com>
2022-05-20 19:42:31 -0300

    linted

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,11 +18,12 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    //'@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
   },
   settings: {
     react: {
